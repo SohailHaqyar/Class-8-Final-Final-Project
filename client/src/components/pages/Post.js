ab998287df68a46b8edf640427f4162235e1c0d7
Sohail Haqyar <haqyarsohail@gmail.com>
2020-07-03 18:18:07 +0300

    added the design and functionalitiy ready for the post routes

--- client/src/components/pages/Post.js
+++ client/src/components/pages/Post.js
@@ -1,18 +1,57 @@
 import React, { Component } from "react";
-import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 import { Link } from "react-router-dom";
+import "./post.styles.scss";
+import axios from "axios";
 
 export class Post extends Component {
+  state = {
+    title: "",
+    body: "",
+  };
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //axios requrest
+  };
   render() {
     return (
-      <div>
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/home">Home</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>Post Feed</BreadcrumbItem>
-        </Breadcrumb>
-        <h1>I also take this to be the post feed</h1>
+      <div className="Post">
+        <div className="Post__compose">
+          <div className="Post__compose-box">
+            <h5 style={{ textAlign: "center", fontWeight: "bold" }}>
+              What's new?
+            </h5>
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup>
+                <Label for="title">Title</Label>
+                <Input
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                  type="text"
+                  name="title"
+                  id="title"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleText">Post Body</Label>
+                <Input
+                  value={this.state.body}
+                  type="textarea"
+                  onChange={this.handleChange}
+                  name="body"
+                  id="exampleText"
+                  style={{ height: "100px" }}
+                />
+              </FormGroup>
+              <Button color="danger" style={{ fontWeight: "bold" }}>
+                Submit
+              </Button>
+            </Form>
+          </div>
+        </div>
       </div>
     );
   }
