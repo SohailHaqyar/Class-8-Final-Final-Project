04b98cf2a1219a5b54db9908e76b635628af9db9
NajeemB <najeemb18@gmail.com>
2020-07-03 19:09:54 +0300

    Update Login.js
    Client_login_commit_1

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,14 +1,27 @@
-import React, { Component } from "react";
-
-export class Login extends Component {
-  render() {
-    return (
-      <div>
-        Try to login here only if your have fully register if not don't try it.
-        If you do .... Bullet bom bom.
-      </div>
-    );
-  }
-}
-
-export default Login;
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { SET_CURRENT_USER } from "./types";
+export const loginUser = data => dispatch => {
+  axios
+    .post("http://localhost:4000/login", data)
+    .then(res => {
+      const { token } = res.data;
+      if (rememberMe) {
+          localStorage.setItem("jwtToken", token);
+      }
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(e => console.log(e));
+};
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem("jwtToken");
+  setAuthToken(false);
+  dispatch(setCurrentUser({}));
+};
+export const setCurrentUser = decoded => ({
+  type: SET_CURRENT_USER,
+  payload: decoded
+});
