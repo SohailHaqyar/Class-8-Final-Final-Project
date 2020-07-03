0f7f6a9a6cee0981e90b3e383984323ea5aedc43
Sohail Haqyar <haqyarsohail@gmail.com>
2020-07-03 19:46:54 +0300

    designed the primal dashboard with static content

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,21 +1,40 @@
 import React, { Component } from "react";
-import { Breadcrumb, BreadcrumbItem } from "reactstrap";
-import { Link } from "react-router-dom";
-
+import "./dashboard.styles.scss";
+import { Button } from "reactstrap";
 export class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/home">Home</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>Dashboard</BreadcrumbItem>
-        </Breadcrumb>
-        <h1>Is this not the Dashboard ? Don't ask me.</h1>
+      <div className="Dashboard">
+        <div className="Dashboard__card">
+          <div className="Dashboard__card-mini">
+            <img
+              src="https://files.slack.com/files-pri/T6RAP57AP-F016Q0QTY68/mylogo.png"
+              alt="logo"
+              className="Dashboard__logo"
+            />
+            <h1
+              style={{ marginLeft: "4.5em", color: "white", marginTop: "20px" }}
+            >
+              Future Tech
+            </h1>
+
+            <div className="Dashboard__card-mini-cta">
+              <Button color="info" style={{ marginLeft: "10px" }}>
+                Edit Profile
+              </Button>
+              <Button color="danger" style={{ marginLeft: "10px" }}>
+                Delete Profile
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        <h1 style={{ marginTop: "200px" }}>Posts will go here</h1>
       </div>
     );
   }
 }
 
 export default Dashboard;
+
+//Cover shoto https://images.unsplash.com/photo-1487088678257-3a541e6e3922
