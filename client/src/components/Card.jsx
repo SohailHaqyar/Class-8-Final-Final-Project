540bf98385eade36a4f501782899e4105b245563
Sohail Haqyar <haqyarsohail@gmail.com>
2020-07-13 20:39:19 +0300

    added not so dynamic post cards waiting for login

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+const Card = () => {
+  return (
+    <div className="Post__feed__Card">
+      <div className="Post__feed__Card__heading">
+        <img src="https://source.unsplash.com/600x400/?person/2" alt="" />
+        <h4>Social Hackers Academy</h4>
+        <p>12 days ago</p>
+      </div>
+      <div className="Post__feed__Card__body">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          doloribus accusantium expedita debitis voluptatibus dolores, saepe
+          nobis iste veritatis eveniet, omnis recusandae adipisci a voluptas!
+          Dolorum aperiam reprehenderit in odit consequatur, omnis sapiente
+          consectetur praesentium nostrum culpa obcaecati vitae repudiandae quis
+          numquam inventore ullam ad hic nulla debitis doloribus optio, nisi eum
+          cupiditate. Nisi sequi molestiae blanditiis molestias voluptates non
+          enim vitae
+        </p>
+        <img src="http://source.unsplash.com/1600x800/?waterfall" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
